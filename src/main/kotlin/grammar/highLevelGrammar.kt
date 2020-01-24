a525c38f6dbd09d04769b2cae2df117f2851db1e
Lancelot Blanchard <lb4218@ic.ac.uk>
2020-01-24 13:50:52 +0000

    Finish updating the high level grammar

--- src/main/kotlin/grammar/highLevelGrammar.kt
+++ src/main/kotlin/grammar/highLevelGrammar.kt
@@ -2,11 +2,13 @@ package ic.org.grammar
 
 import org.intellij.lang.annotations.Identifier
 
+// <program>
 data class Prog(
   val funcs: List<Func>,
   val firstStat: Stat
 )
 
+// <func>
 data class Func(
   val retType: Type,
   val ident: Ident,
@@ -14,16 +16,10 @@ data class Func(
   val stat: Stat
 )
 
-sealed class Type
-sealed class BaseT : Type()
-object IntT : BaseT()
-object BoolT : BaseT()
-object CharT : BaseT()
-object StringT : BaseT()
+// <param>
+data class Param(val type: Type, val ident: Ident)
 
-object ArrayT : Type()
-object PairT : Type()
-
+// <stat>
 sealed class Stat
 object Skip : Stat()
 data class Decl(val type: Type, val id: Ident, val rhs: AssRHS) : Stat()
@@ -39,30 +35,44 @@ data class While(val cond: Expr, val stat: Stat) : Stat()
 data class BegEnd(val stat: Expr) : Stat()
 data class StatChain(val stat1: Stat, val stat2: Stat) : Stat()
 
-sealed class AssRHS
-data class ArrayLit(val exprs: List<Expr>) : AssRHS()
-data class Newpair(val expr1: Expr, val expr2: Expr) : AssRHS()
-data class Call(val id: Ident, val args: ArgList) : AssRHS()
-
-data class ExprRHS(val expr: Expr) : AssRHS()
-
-// TODO try something cleaner
-interface PairElem
-sealed class PairElemRHS() : PairElem, AssRHS()
-sealed class PairElemLHS() : PairElem, AssLHS()
-
-data class Fst(val expr: Expr) : PairElem
-data class Snd(val expr: Expr) : PairElem
-
+// <assign-lhs>
 sealed class AssLHS
 data class IdentLHS(val ident: Ident) : AssLHS()
+data class ArrauElemLHS(val arrayElem: ArrayElem) : AssLHS()
+data class PairElemLHS(val pairElem: PairElem) : AssLHS()
 
-sealed class ArgList
+// <assign-rhs>
+sealed class AssRHS
+data class ExprRHS(val expr: Expr) : AssRHS()
+data class ArrayLit(val exprs: List<Expr>) : AssRHS()
+data class Newpair(val expr1: Expr, val expr2: Expr) : AssRHS()
+data class PairElemRHS(val pairElem: PairElem) : AssRHS()
+data class Call(val id: Ident, val args: List<Expr>) : AssRHS()
 
-data class Param(val type: Type, val ident: Ident)
+// <pair-elem>
+sealed class PairElem
+data class Fst(val expr: Expr) : PairElem()
+data class Snd(val expr: Expr) : PairElem()
 
-data class Ident(val name: String)
+// <type>
+sealed class Type
+sealed class BaseT : Type()
+data class ArrayT(val type: Type) : Type()
+data class PairT(val fstT: PairElemT, val sndT: PairElemT) : Type()
 
+// <base-type>
+object IntT : BaseT()
+object BoolT : BaseT()
+object CharT : BaseT()
+object StringT : BaseT()
+
+// <pair-elem-type>
+sealed class PairElemT
+data class PairElemBaseT(val baseType: BaseT) : PairElemT()
+data class PairElemArrayT(val arrayType: ArrayT) : PairElemT()
+object NDPairT : PairElemT()
+
+// <expr>
 sealed class Expr
 data class IntExpr(val value: Int) : Expr()
 data class BoolExpr(val value: Boolean) : Expr()
@@ -70,10 +80,7 @@ data class CharExpr(val value: Char) : Expr()
 data class StrExpr(val value: String) : Expr()
 object NullPairExpr : Expr()
 data class IdentExpr(val ident: Ident) : Expr()
-data class ArrayElemExpr(
-  val ident: Ident,
-  val indices: List<Expr>
-) : Expr()
+data class ArrayElemExpr(val arrayElem: ArrayElem) : Expr()
 data class UnaryOperExpr(
   val unaryOper: UnaryOper,
   val expr: Expr
@@ -84,6 +91,7 @@ data class BinaryOperExpr(
   val expr2: Expr
 ) : Expr()
 
+// <unary-oper>
 sealed class UnaryOper
 object NotUO : UnaryOper()       // !
 object MinusUO : UnaryOper()     // -
@@ -91,6 +99,7 @@ object LenUO : UnaryOper()       // len
 object OrdUO : UnaryOper()       // ord
 object ChrUO : UnaryOper()       // chr
 
+// <binary-oper>
 sealed class BinaryOper
 object TimesBO : BinaryOper()    // *
 object DivisionBO : BinaryOper() // /
@@ -105,3 +114,15 @@ object EqBO : BinaryOper()       // ==
 object NeqBO : BinaryOper()      // !=
 object AndBO : BinaryOper()      // &&
 object OrBO : BinaryOper()       // ||
+
+// <ident>
+data class Ident(val name: String)
+
+// <array-elem>
+data class ArrayElem(
+  val ident: Ident,
+  val indices: List<Expr>
+)
+
+// <comment>
+data class Comment(val comment: String)
