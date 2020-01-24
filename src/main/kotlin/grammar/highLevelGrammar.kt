d31a0a7009f1b2720393e1c2eff990ec63d99fcd
Lancelot Blanchard <lb4218@ic.ac.uk>
2020-01-24 09:44:37 +0000

    Add Expr class to High Level Grammar in Kotlin

--- src/main/kotlin/grammar/highLevelGrammar.kt
+++ src/main/kotlin/grammar/highLevelGrammar.kt
@@ -24,5 +24,49 @@ object PairT : Type()
 
 sealed class Stat
 
-data class Ident(val name: String)
 data class Param(val type: Type, val ident: Ident)
+
+data class Ident(val name: String)
+
+sealed class Expr
+data class IntExpr(val value: Int) : Expr()
+data class BoolExpr(val value: Boolean) : Expr()
+data class CharExpr(val value: Char) : Expr()
+data class StrExpr(val value: String) : Expr()
+object NullPairExpr : Expr()
+data class IdentExpr(val ident: Ident) : Expr()
+data class ArrayElemExpr(
+  val ident: Ident,
+  val indices: List<Expr>
+) : Expr()
+data class UnaryOperExpr(
+  val unaryOper: UnaryOper,
+  val expr: Expr
+) : Expr()
+data class BinaryOperExpr(
+  val expr1: Expr,
+  val binaryOper: BinaryOper,
+  val expr2: Expr
+) : Expr()
+
+sealed class UnaryOper
+object NotUO : UnaryOper()
+object MinusUO : UnaryOper()
+object LenUO : UnaryOper()
+object OrdUO : UnaryOper()
+object ChrUO : UnaryOper()
+
+sealed class BinaryOper
+object TimesBO : BinaryOper()
+object DivisionBO : BinaryOper()
+object ModBO : BinaryOper()
+object PlusBO : BinaryOper()
+object MinusBO : BinaryOper()
+object GtBO : BinaryOper()
+object GeqBO : BinaryOper()
+object LtBO : BinaryOper()
+object LeqBO : BinaryOper()
+object EqBO : BinaryOper()
+object NeqBO : BinaryOper()
+object AndBO : BinaryOper()
+object OrBO : BinaryOper()
