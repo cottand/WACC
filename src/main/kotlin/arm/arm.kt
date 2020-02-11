c394ba0379692c28765cd1db10845d118e7013d8
Lancelot Blanchard <lancelotblanchard@dyn3178-61.wlan.ic.ac.uk>
2020-02-11 12:12:54 +0000

    Add condition flags

--- src/main/kotlin/arm/arm.kt
+++ src/main/kotlin/arm/arm.kt
@@ -13,3 +13,23 @@ sealed class ARMInstr {
     return code()
   }
 }
+
+sealed class CondFlag
+
+object EQCond : CondFlag() // Equal
+object NECond : CondFlag() // Not Equal
+object HSCond : CondFlag() // Unsigned higher or same
+object CSCond : CondFlag() // Carry Set
+object LOCond : CondFlag() // Unsigned lower
+object CCCond : CondFlag() // Carry clear
+object MICond : CondFlag() // Negative, Minus
+object PLCond : CondFlag() // Positive or zero, plus
+object VSCond : CondFlag() // Overflow
+object VCCond : CondFlag() // No overflow
+object HICond : CondFlag() // Unsigned higher
+object LSCond : CondFlag() // Unsigned lower or same
+object GECond : CondFlag() // Signed greater or equal
+object LTCond : CondFlag() // Signed less than
+object GTCond : CondFlag() // Signed greater than
+object LECond : CondFlag() // Signed less than or equal
+object ALCond : CondFlag() // Always
