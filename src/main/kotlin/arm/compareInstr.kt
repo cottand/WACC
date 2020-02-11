d6272861c42051843a11d930f5fb511271ea23a9
William Profit <williamprofit@hotmail.fr>
2020-02-11 16:49:21 +0000

    Implement Compare instructions

---
+++ src/main/kotlin/arm/compareInstr.kt
@@ -0,0 +1,10 @@
+package ic.org.arm
+
+import arrow.core.Option
+
+data class CMPInstr(override val cond: Option<CondFlag>, val rn: Reg, val op2: Operand2) : ARMInstr(cond) {
+  override val code = "CMP$condStr ${rn.code}, ${op2.code}"
+}
+data class CMNInstr(override val cond: Option<CondFlag>, val rn: Reg, val op2: Operand2) : ARMInstr(cond) {
+  override val code = "CMN$condStr ${rn.code}, ${op2.code}"
+}
