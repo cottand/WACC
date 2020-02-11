e1699c7c612d1c7ef97b6b4e36003d009ac97cd6
William Profit <williamprofit@hotmail.fr>
2020-02-11 16:34:19 +0000

    Implement Logical instructions

--- src/main/kotlin/arm/arm.kt
+++ src/main/kotlin/arm/arm.kt
@@ -9,7 +9,9 @@ interface Printable {
   val code: String
 }
 
-abstract class ARMInstr(val cond: Option<CondFlag>) : Printable {
+abstract class ARMInstr(open val cond: Option<CondFlag>) : Printable {
+  val condStr = cond.fold({ "" }, { it.code })
+
   /**
    * Aliases to [Printable.code]
    */
@@ -19,23 +21,3 @@ abstract class ARMInstr(val cond: Option<CondFlag>) : Printable {
 data class Reg(var id : Int) : Printable {
   override val code = "r$id"
 }
-
-sealed class CondFlag
-
-object EQCond : CondFlag() // Equal
-object NECond : CondFlag() // Not Equal
-object HSCond : CondFlag() // Unsigned higher or same
-object CSCond : CondFlag() // Carry Set
-object LOCond : CondFlag() // Unsigned lower
-object CCCond : CondFlag() // Carry clear
-object MICond : CondFlag() // Negative, Minus
-object PLCond : CondFlag() // Positive or zero, plus
-object VSCond : CondFlag() // Overflow
-object VCCond : CondFlag() // No overflow
-object HICond : CondFlag() // Unsigned higher
-object LSCond : CondFlag() // Unsigned lower or same
-object GECond : CondFlag() // Signed greater or equal
-object LTCond : CondFlag() // Signed less than
-object GTCond : CondFlag() // Signed greater than
-object LECond : CondFlag() // Signed less than or equal
-object ALCond : CondFlag() // Always
