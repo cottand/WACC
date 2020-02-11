dbc67f49600576c8913eda4ad0d4080b9374741f
William Profit <williamprofit@hotmail.fr>
2020-02-11 11:51:41 +0000

    Implement super class ARMInstr

--- src/main/kotlin/arm/arm.kt
+++ src/main/kotlin/arm/arm.kt
@@ -1,2 +1,15 @@
 package ic.org.arm
 
+sealed class ARMInstr {
+  /**
+   * Returns the ARM assembly code for the instruction
+   */
+  abstract fun code() : String
+
+  /**
+   * Aliases to ARMInstr::code()
+   */
+  override fun toString(): String {
+    return code()
+  }
+}
