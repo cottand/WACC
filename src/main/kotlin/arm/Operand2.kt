bcb4d69e0b16944f3f84ab4ef9fef11ce772ca8f
William Profit <williamprofit@hotmail.fr>
2020-02-20 12:24:47 +0000

    Add missing comma in Operand2 shit instructions

--- src/main/kotlin/arm/Operand2.kt
+++ src/main/kotlin/arm/Operand2.kt
@@ -9,34 +9,34 @@ data class ImmOperand2(val imm: Immed_8r) : Operand2() {
   override val code = "#${imm.code}"
 }
 data class LSLImmOperand2(val rm: Register, val imm: Immed_5) : Operand2() {
-  override val code = "${rm.code} LSL #${imm.code}"
+  override val code = "${rm.code}, LSL #${imm.code}"
 }
 data class LSRImmOperand2(val rm: Register, val imm: Immed_5) : Operand2() {
-  override val code = "${rm.code} LSR #${imm.code}"
+  override val code = "${rm.code}, LSR #${imm.code}"
 }
 data class ASRImmOperand2(val rm: Register, val imm: Immed_5) : Operand2() {
-  override val code = "${rm.code} ASR #${imm.code}"
+  override val code = "${rm.code}, ASR #${imm.code}"
 }
 data class RORImmOperand2(val rm: Register, val imm: Immed_5) : Operand2() {
-  override val code = "${rm.code} ROR #${imm.code}"
+  override val code = "${rm.code}, ROR #${imm.code}"
 }
 data class RegOperand2(val rm: Register) : Operand2() {
   override val code = rm.code
 }
 data class LSLRegOperand2(val rm: Register, val rs: Register) : Operand2() {
-  override val code = "${rm.code} LSL ${rs.code}"
+  override val code = "${rm.code}, LSL ${rs.code}"
 }
 data class LSRRegOperand2(val rm: Register, val rs: Register) : Operand2() {
-  override val code = "${rm.code} LSR ${rs.code}"
+  override val code = "${rm.code}, LSR ${rs.code}"
 }
 data class ASRRegOperand2(val rm: Register, val rs: Register) : Operand2() {
-  override val code = "${rm.code} ASR ${rs.code}"
+  override val code = "${rm.code}, ASR ${rs.code}"
 }
 data class RORRegOperand2(val rm: Register, val rs: Register) : Operand2() {
-  override val code = "${rm.code} ROR ${rs.code}"
+  override val code = "${rm.code}, ROR ${rs.code}"
 }
 data class RRXOperand2(val rm: Register) : Operand2() {
-  override val code = "${rm.code} RRX"
+  override val code = "${rm.code}, RRX"
 }
 
 
