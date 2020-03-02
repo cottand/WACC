5087d04685a50bb4f745e5f211c33f45182c79e9
William Profit <williamprofit@hotmail.fr>
2020-03-02 14:19:38 +0000

    Implement jvm arithmetic instructions

---
+++ src/main/kotlin/jvm/arithmetic.kt
@@ -0,0 +1,17 @@
+package ic.org.jvm
+
+class IADD() : JvmInstr {
+  override val code = "iadd"
+}
+
+class IDIV() : JvmInstr {
+  override val code = "idiv"
+}
+
+class IMUL() : JvmInstr {
+  override val code = "imul "
+}
+
+class ISUB() : JvmInstr {
+  override val code = "isub "
+}
