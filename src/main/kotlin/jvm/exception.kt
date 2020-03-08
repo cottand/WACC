3b8ecf71e840f492974427a4af7858df38cd9e6b
William Profit <williamprofit@hotmail.fr>
2020-03-08 12:49:26 +0000

    Check for underflow for binary operators

--- src/main/kotlin/jvm/exception.kt
+++ src/main/kotlin/jvm/exception.kt
@@ -12,8 +12,10 @@ fun JvmCheckIntegerOverflow(op : BinaryOper) = JvmAsm {
   val msg = "Integer Overflow"
   val exception = "java/lang/RuntimeException"
   val method = "java/lang/RuntimeException/<init>(Ljava/lang/String;)V"
-  val labelSkip = JvmLabel("L_OVERFLOW_SKIP_" + shortRandomUUID())
+  val labelSkipOF = JvmLabel("L_OVERFLOW_SKIP_" + shortRandomUUID())
+  val labelSkipUF = JvmLabel("L_UNDERFLOW_SKIP_" + shortRandomUUID())
 
+  // Check for overflow
   +DUP2
   +DUP_X1
   +I2L
@@ -32,11 +34,38 @@ fun JvmCheckIntegerOverflow(op : BinaryOper) = JvmAsm {
   +LDC(2147483647)
   +I2L
   +LCMP
-  +IFLE(labelSkip)
+  +IFLE(labelSkipOF)
   +NEW(exception)
   +DUP
   +LDC(msg)
   +InvokeSpecial(method)
   +ATHROW
-  +labelSkip
+  +labelSkipOF
+
+  // Check for underflow
+  +DUP2
+  +DUP_X1
+  +I2L
+  +SWAP2
+  +SWAP
+  +POP
+  +I2L
+
+  +when (op) {
+    is MulBO -> LMUL
+    is PlusBO -> LADD
+    is MinusBO -> LSUB
+    else -> NOT_REACHED()
+  }
+
+  +LDC(-2147483648)
+  +I2L
+  +LCMP
+  +IFGE(labelSkipUF)
+  +NEW(exception)
+  +DUP
+  +LDC(msg)
+  +InvokeSpecial(method)
+  +ATHROW
+  +labelSkipUF
 }
