f0cb6cb34cb9852d63574118b8951881dcd40cba
Nico D'Cotta <nico.dc@outlook.com>
2020-03-12 12:23:33 +0000

    Reformat Stack instructions and branch.kt

---
+++ src/main/kotlin/jvm/StackInstr.kt
@@ -0,0 +1,21 @@
+package ic.org.jvm
+
+sealed class StackInstr(override val code: String) : JvmInstr
+
+object DUP : StackInstr("dup")
+
+object DUP2 : StackInstr("dup2")
+
+object POP : StackInstr("pop")
+
+object POP2 : StackInstr("pop2")
+
+object SWAP : StackInstr("swap")
+
+// Swap2 is equivalent to dup2_x2 then pop_2
+object SWAP2 : StackInstr(DUP2_X2.code + '\n' + POP2.code)
+
+object DUP_X1 : StackInstr("dup_x1")
+
+object DUP2_X2 : StackInstr("dup2_x2")
+
--- src/main/kotlin/jvm/branch.kt
+++ src/main/kotlin/jvm/branch.kt
@@ -1,68 +1,42 @@
+@file:Suppress("ClassName")
+
 package ic.org.jvm
 
-data class GOTO(val label: JvmLabel) : JvmInstr {
-  override val code = "goto ${label.name}"
+sealed class BranchInstr(mnemonic: String, label: JvmLabel) : JvmInstr {
+  override val code = "$mnemonic ${label.name}"
 }
 
-data class IFEQ(val label: JvmLabel) : JvmInstr {
-  override val code = "ifeq ${label.name}"
-}
+data class GOTO(val label: JvmLabel) : BranchInstr("goto", label)
 
-data class IFGE(val label: JvmLabel) : JvmInstr {
-  override val code = "ifge ${label.name}"
-}
+data class IFEQ(val label: JvmLabel) : BranchInstr("ifeq", label)
 
-data class IFGT(val label: JvmLabel) : JvmInstr {
-  override val code = "ifgt ${label.name}"
-}
+data class IFGE(val label: JvmLabel) : BranchInstr("ifge", label)
 
-data class IFLE(val label: JvmLabel) : JvmInstr {
-  override val code = "ifle ${label.name}"
-}
+data class IFGT(val label: JvmLabel) : BranchInstr("ifgt", label)
 
-data class IFLT(val label: JvmLabel) : JvmInstr {
-  override val code = "iflt ${label.name}"
-}
+data class IFLE(val label: JvmLabel) : BranchInstr("ifle", label)
 
-data class IFNE(val label: JvmLabel) : JvmInstr {
-  override val code = "ifne ${label.name}"
-}
+data class IFLT(val label: JvmLabel) : BranchInstr("iflt", label)
 
-data class IF_ICMPEQ(val label: JvmLabel) : JvmInstr {
-  override val code = "if_icmpeq ${label.name}"
-}
+data class IFNE(val label: JvmLabel) : BranchInstr("ifne", label)
 
-data class IF_ICMPNE(val label: JvmLabel) : JvmInstr {
-  override val code = "if_icmpne ${label.name}"
-}
+data class IF_ICMPEQ(val label: JvmLabel) : BranchInstr("if_icmpeq", label)
 
-data class IF_ICMPGE(val label: JvmLabel) : JvmInstr {
-  override val code = "if_icmpge ${label.name}"
-}
+data class IF_ICMPNE(val label: JvmLabel) : BranchInstr("if_icmpne", label)
 
-data class IF_ICMPGT(val label: JvmLabel) : JvmInstr {
-  override val code = "if_icmpgt ${label.name}"
-}
+data class IF_ICMPGE(val label: JvmLabel) : BranchInstr("if_icmpge", label)
 
-data class IF_ICMPLE(val label: JvmLabel) : JvmInstr {
-  override val code = "if_icmple ${label.name}"
-}
+data class IF_ICMPGT(val label: JvmLabel) : BranchInstr("if_icmpgt", label)
 
-data class IF_ICMPLT(val label: JvmLabel) : JvmInstr {
-  override val code = "if_icmplt ${label.name}"
-}
+data class IF_ICMPLE(val label: JvmLabel) : BranchInstr("if_icmple", label)
 
-data class IF_ACMPEQ(val label: JvmLabel) : JvmInstr {
-  override val code = "if_acmpeq ${label.name}"
-}
+data class IF_ICMPLT(val label: JvmLabel) : BranchInstr("if_icmplt", label)
 
-data class IF_ACMPNE(val label: JvmLabel) : JvmInstr {
-  override val code = "if_acmpne ${label.name}"
-}
+data class IF_ACMPEQ(val label: JvmLabel) : BranchInstr("if_acmpeq", label)
 
-data class IFNONNULL(val label: JvmLabel) : JvmInstr {
-  override val code = "ifnonnull ${label.name}"
-}
+data class IF_ACMPNE(val label: JvmLabel) : BranchInstr("if_acmpne", label)
+
+data class IFNONNULL(val label: JvmLabel) : BranchInstr("ifnonnull", label)
 
 object LCMP : JvmInstr {
   override val code = "lcmp"
